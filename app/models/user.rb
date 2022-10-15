a9cc4f391834c88eb2d9e6c29646854035e48af7
nudroban <nudrob557@gmail.com>
2022-10-15 14:24:32 +0300

    add phone login method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,22 @@
+# frozen_string_literal: true
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, authentication_keys: [:login]
+
+  attr_writer :login
+
+  def login
+    @login || phone || email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if (login = conditions.delete(:login))
+      where(conditions.to_h).where(["lower(phone) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    elsif conditions.has_key?(:phone) || conditions.has_key?(:email)
+      where(conditions.to_h).first
+    end
+  end
 end
