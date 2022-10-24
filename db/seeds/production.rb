8d8cc7c67fbf4b6f51626bfcf51535bc91825925
nudroban <nudrob557@gmail.com>
2022-10-24 03:28:16 +0300

    fix database

--- db/seeds/production.rb
+++ db/seeds/production.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 ActiveRecord::Base.transaction do
-  AdminUser.create!(email: 'adminadmin@mail.ua', phone: '0507387961', password: 'password')
+  AdminUser.create!(email: 'adminadmin@mail.ua', phone: '0507387900', password: 'password')
   categories = %w[dantist therapevt family-doctor lor]
   categories.each do |category|
-    Category.create!(name: "#{category}")
+    Category.create(name: "#{category}")
   end
-  Doctor.create!(name: "Bishop", email: "bishop@mail.ua", phone: '0661234567', password: 'qwqwqw', category_id: 1)
-  Doctor.create!(name: "Evil", email: "evil@mail.ua", phone: '0661234568', password: 'qwqwqw', category_id: 1)
-  Doctor.create!(name: "Melvin", email: "Melvin@mail.ua", phone: '0661234568', password: 'qwqwqw', category_id: 2)
-  Doctor.create!(name: "Darvin", phone: '0661234568', password: 'qwqwqw', category_id: 3)
+  Doctor.create(name: "Bishop", email: "bishop@mail.ua", phone: '0661234567', password: 'qwqwqw', category_id: 1)
+  Doctor.create(name: "Evil", email: "evil@mail.ua", phone: '0661234568', password: 'qwqwqw', category_id: 1)
+  Doctor.create(name: "Melvin", email: "Melvin@mail.ua", phone: '0661234568', password: 'qwqwqw', category_id: 2)
+  Doctor.create(name: "Darvin", phone: '0661234568', password: 'qwqwqw', category_id: 3)
 end
