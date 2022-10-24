5015284b7123bf19fbef5e9de4028cdabb69abd6
nudroban <nudrob557@gmail.com>
2022-10-24 03:13:13 +0300

    add category and doc in seed

--- db/seeds/production.rb
+++ db/seeds/production.rb
@@ -1,3 +1,13 @@
+# frozen_string_literal: true
+
 ActiveRecord::Base.transaction do
-  AdminUser.create!(email: "admin@mail.ua", phone: '0507387960', password: 'password')
+  AdminUser.create!(email: 'admin@mail.ua', phone: '0507387960', password: 'password')
+  categories = %w[dantist therapevt family-doctor lor]
+  categories.each do |category|
+    Category.create!(name: "#{category}")
+  end
+  Doctor.create!(name: "Bishop", email: "bishop@mail.ua", phone: '0661234567', password: 'qwqwqw', category_id: 1)
+  Doctor.create!(name: "Evil", email: "evil@mail.ua", phone: '0661234568', password: 'qwqwqw', category_id: 1)
+  Doctor.create!(name: "Melvin", email: "Melvin@mail.ua", phone: '0661234568', password: 'qwqwqw', category_id: 2)
+  Doctor.create!(name: "Darvin", phone: '0661234568', password: 'qwqwqw', category_id: 3)
 end
